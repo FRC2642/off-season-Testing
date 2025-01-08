3e236df215d79b664275f6386ffe3eda53bacfe2
Ian Jones <120682566+Astronomy-Daze@users.noreply.github.com>
2025-01-08 00:11:13 +0000

    Limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -195,6 +195,7 @@ public class Limelight extends SubsystemBase {
   }
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    limelightDiagnostic();
+    update2DMeasurements();
   }
 }
